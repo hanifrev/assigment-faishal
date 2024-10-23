6272803adf56ab19e713cd7cb7d9441cf01851fc
Faishal Hanif <hanifrev@gmail.com>
2024-10-24 00:26:13 +0700

    remove api calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
 import Homepage from "@/modules/Homepage";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
+// const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
-const getData = async () => {
-  const response = await fetch(`${BASE_URL}/api/homepage`);
+// const getData = async () => {
+//   const response = await fetch(`${BASE_URL}/api/homepage`);
 
-  if (!response.ok) {
-    throw new Error("ERROR while getting data");
-  }
+//   if (!response.ok) {
+//     throw new Error("ERROR while getting data");
+//   }
 
-  const result = await response.json();
-  return result;
-};
+//   const result = await response.json();
+//   return result;
+// };
 
 export default async function Home() {
-  if (!BASE_URL) {
-    return null;
-  }
+  // if (!BASE_URL) {
+  //   return null;
+  // }
 
-  const data = await getData();
+  // const data = await getData();
 
-  if (!data) {
-    return null;
-  }
+  // if (!data) {
+  //   return null;
+  // }
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <div>
